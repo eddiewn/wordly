854bf0cbe24f43caf7bd517399fb89259b10d6c5
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-09-28 11:48:53 +0200

    Added backend server with "database" of 20 5 word words. Also prints out a 5x7 playing field with last row being input fields

---
+++ server.ts
@@ -0,0 +1,39 @@
+import express from "express";
+const app = express();
+
+import cors from "cors";
+
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
+
+// My makeshift database
+const words = [
+    "apple",
+    "grape",
+    "peach",
+    "mango",
+    "lemon",
+    "berry",
+    "melon",
+    "plums",
+    "chest",
+    "pearl",
+    "olive",
+    "apric",
+    "cider",
+    "guava",
+    "figgy",
+    "prune",
+    "hazel",
+    "honey",
+    "sugar",
+    "spice"
+];
+
+app.get("/api/givemeWOOORD", (req, res) => {
+    const randomWord = words[Math.floor(Math.random() * words.length)];
+    res.json({ word: randomWord });
+});
+
+app.listen(4000, () => console.log("Server running on port 4000"));
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
-function App() {
+import { useState, useEffect } from "react"
 
-  return (
+function App() {
+    const [word, setWord] = useState<string>("");
+
+    useEffect(() => {
+        const fetchWord = async () => {
+            try {
+                const response = await fetch("http://localhost:4000/api/givemeWOOORD");
+                const data = await response.json();
+                setWord(data.word);
+
+                console.log("word:", data.word);
+            } catch (error) {
+                console.error("Error fetching word:", error);
+            }
+        };
+        fetchWord();
+    }, [])
+
+return (
     <>
-        <div className="text-3xl font-bold underline">
-            Hello world!
+        <div className="w-[20%] grid grid-cols-5 grid-rows-7 gap-1.5">
+            {Array.from({ length: 35 }).map((_, i) => (
+                i < 30
+                ?
+                <div key={i} className="border-2 border-black bg-amber-300 aspect-square">
+                    <p></p>
+                </div>
+                : 
+                <div key={i} className="bg-blue-300 border-2 border-black aspect-square">
+                    <input type="text" maxLength={1} className="w-full h-full text-center uppercase"/>
+                </div>
+        ))}
+        </div>
+        <div>
+            {word}
         </div>
     </>
   )
