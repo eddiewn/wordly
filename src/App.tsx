7dcd19c6718c64b11248f87e5ef7e4e261e1b491
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-09-28 14:59:09 +0200

    Made DisplayGrid into a component and some other stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useState, useEffect} from "react"
+import DisplayGrid from "./components/DisplayGrid"
+
 
 function App() {
     const [word, setWord] = useState<string>("");
@@ -13,7 +15,7 @@ function App() {
                 const data = await response.json();
                 setWord(data.word);
 
-                console.log("word:", data.word);
+                console.log("Word:", data.word);
             } catch (error) {
                 console.error("Error fetching word:", error);
             }
@@ -21,6 +23,10 @@ function App() {
         fetchWord();
     }, [])
 
+
+useEffect(() => {
+    console.log("All guesses", guesses);
+}, [guesses]);
 
 function getAttemptRange(a: number) {
     const start = (a - 1) * 5 + 1;
@@ -44,8 +50,8 @@ function getAttemptRange(a: number) {
                         body: JSON.stringify({ guess: currentGuess.join("")})
                     });
                     const data = await response.json();
-                    console.log("Guess response:", data);
                     setGuesses(data.guesses);
+                    setCurrentGuess([]);
                 } catch (error) {
                     console.error("Error posting guess:", error);
                 }
@@ -64,19 +70,7 @@ function getAttemptRange(a: number) {
 
 return (    
     <>
-        <div  className="w-[20%] grid grid-cols-5 grid-rows-7 gap-1.5 ">
-            {Array.from({ length: 30 }).map((_, i) => (
-                i+1 >= getAttemptRange(attempt).start && i < getAttemptRange(attempt).end
-                ?
-                <div key={i} onKeyDown={keyDownFunction} className="border-2 border-black bg-amber-300 aspect-square">
-                    <input type="text" maxLength={1}  className="w-full h-full text-center uppercase"/>
-                </div>
-                : 
-                <div key={i} className="flex items-center justify-center border-2 border-black bg-blue-300  aspect-square">
-                    <p className="text-center uppercase m-auto">no</p>
-                </div>
-        ))}
-        </div>
+        <DisplayGrid attempt={attempt} keyDownFunction={keyDownFunction} getAttemptRange={getAttemptRange}/>
         <div>
             {word}
         </div>
---
+++ src/components/DisplayGrid.tsx
@@ -0,0 +1,25 @@
+type Props = {
+    attempt: number;
+    keyDownFunction: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+    getAttemptRange: (a: number) => { start: number; end: number };
+
+}
+
+const DisplayGrid = ({attempt, keyDownFunction, getAttemptRange }: Props) => {
+    return (
+        <div className="w-[20%] grid grid-cols-5 grid-rows-7 gap-1.5 ">
+            {Array.from({ length: 30 }).map((_, i) => (
+                i+1 >= getAttemptRange(attempt).start && i < getAttemptRange(attempt).end
+                ?
+                <div key={i} onKeyDown={keyDownFunction} className="border-2 border-black bg-amber-300 aspect-square">
+                    <input type="text" maxLength={1}  className="w-full h-full text-center uppercase"/>
+                </div>
+                : 
+                <div key={i} className="flex items-center justify-center border-2 border-black bg-blue-300  aspect-square">
+                    <p className="text-center uppercase m-auto">no</p>
+                </div>
+        ))}
+        </div>
+    )
+}
+export default DisplayGrid;
