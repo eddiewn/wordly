576aa5d7984b92155b26805f1720a517486278a7
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-09-28 12:19:26 +0200

    Removed 7th row and made inputs directly on field, now changes depending on artificial attempt

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, use } from "react"
 
 function App() {
     const [word, setWord] = useState<string>("");
+    const [attempt, setAttempt] = useState<number>(1);
 
     useEffect(() => {
         const fetchWord = async () => {
@@ -18,18 +19,27 @@ function App() {
         fetchWord();
     }, [])
 
+
+function getAttemptRange(a: number) {
+    const start = (a - 1) * 5 + 1;
+    const end = a * 5;
+    return { start, end };
+}
+
 return (
+
+    
     <>
         <div className="w-[20%] grid grid-cols-5 grid-rows-7 gap-1.5">
-            {Array.from({ length: 35 }).map((_, i) => (
-                i < 30
+            {Array.from({ length: 30 }).map((_, i) => (
+                i+1 >= getAttemptRange(attempt).start && i < getAttemptRange(attempt).end
                 ?
                 <div key={i} className="border-2 border-black bg-amber-300 aspect-square">
-                    <p></p>
+                    <input type="text" maxLength={1} className="w-full h-full text-center uppercase"/>
                 </div>
                 : 
-                <div key={i} className="bg-blue-300 border-2 border-black aspect-square">
-                    <input type="text" maxLength={1} className="w-full h-full text-center uppercase"/>
+                <div key={i} className="flex items-center justify-center border-2 border-black bg-blue-300  aspect-square">
+                    <p className="text-center uppercase m-auto" >no</p>
                 </div>
         ))}
         </div>
