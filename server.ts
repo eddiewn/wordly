0599b92c9d4af468e80d9ede6007829453cea6c0
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-09-30 11:40:04 +0200

    Now server manages attempts instead of client.

--- server.ts
+++ server.ts
@@ -5,9 +5,11 @@ import cors from "cors";
 
 app.use(express.json());
 
-app.use(cors({
-  origin: "http://localhost:5173"
-}));
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+    })
+);
 
 // My makeshift database
 const words = [
@@ -30,7 +32,7 @@ const words = [
     "hazel",
     "honey",
     "sugar",
-    "spice"
+    "spice",
 ];
 
 const guesses: string[] = [];
@@ -38,26 +40,24 @@ let attempts: number = 1;
 
 app.get("/api/givemeWOOORD", (req, res) => {
     const randomWord = words[Math.floor(Math.random() * words.length)];
-    res.json({ word: randomWord });
+    res.json({word: randomWord});
 });
 
 app.get("/api/guesses", (req, res) => {
-    res.json({ guesses, attempts });
+    res.json({guesses, attempts});
 });
 
 app.post("/api/guesses", (req, res) => {
-    const { guess } = req.body;
-    const { attempt }  = req.body;
-    
-    attempts = attempt + 1;
-    guesses.push(guess)
-    res.json({ guesses, attempts });
+    const {guess} = req.body;
+    attempts += 1;
+    guesses.push(guess);
+    res.json({guesses, attempts});
 });
 
 app.delete("/api/guesses", (req, res) => {
     guesses.length = 0;
     attempts = 1;
-    res.json({ message: "Guesses and attempts reset" });
-}); 
+    res.json({message: "Guesses and attempts reset"});
+});
 
 app.listen(4000, () => console.log("Server running on port 4000"));
