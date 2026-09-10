43acbab9456f7b7f2996a53185f4e01ac04c0713
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2026-09-10 13:13:24 +0200

    render port

--- server.ts
+++ server.ts
@@ -183,5 +183,6 @@ app.post("/api/guesses", (req, res) => {
     }
 });
 
+const PORT = process.env.PORT || 4000
 
-app.listen(4000, "0.0.0.0", () => console.log("Server running on port 4000"));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT} `));
